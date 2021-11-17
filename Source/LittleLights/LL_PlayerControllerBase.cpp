a27555442326628fd73c8623fae4547eca21163d
Josue G.Z <iosue27@gmail.com>
2021-11-16 23:28:41 -0600

    Menu UI updates
    -Main Menu changes to use BP widgest
    -Playercontroller added functions for UI functionality and pause menu

--- Source/LittleLights/LL_PlayerControllerBase.cpp
+++ Source/LittleLights/LL_PlayerControllerBase.cpp
@@ -3,7 +3,9 @@
 
 #include "LL_PlayerControllerBase.h"
 
+
 #include "Level_Manager_Base.h"
+#include "Blueprint/UserWidget.h"
 #include "Kismet/GameplayStatics.h"
 
 void ALL_PlayerControllerBase::BeginPlay()
@@ -11,4 +13,49 @@ void ALL_PlayerControllerBase::BeginPlay()
 	Super::BeginPlay();
 
 
+}
+
+void ALL_PlayerControllerBase::DisableAllInput()
+{
+	DisableInput(this);
+
+	bShowMouseCursor = false;
+}
+
+void ALL_PlayerControllerBase::ChangeToGameInput()
+{
+	SetInputMode(FInputModeGameOnly());
+	bShowMouseCursor = false;
+
+}
+
+void ALL_PlayerControllerBase::ChangeToUIInput()
+{
+	SetInputMode(FInputModeUIOnly());
+	bShowMouseCursor = true;
+
+}
+
+void ALL_PlayerControllerBase::TogglePauseMenu()
+{
+	if(PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr;
+		ChangeToGameInput();
+		return;
+	}
+	PauseMenuInstance = CreateWidget(this,PauseMenuClass);
+	if(PauseMenuInstance)
+	{
+		PauseMenuInstance->AddToViewport(100);
+		ChangeToUIInput();
+	}
+}
+
+void ALL_PlayerControllerBase::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("PauseMenu",IE_Pressed,this,&ALL_PlayerControllerBase::TogglePauseMenu);
 }
--- Source/LittleLights/LL_PlayerControllerBase.h
+++ Source/LittleLights/LL_PlayerControllerBase.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "LL_PlayerControllerBase.generated.h"
 
+class UUserWidget;
 /**
  * 
  */
@@ -15,7 +16,27 @@ class LITTLELIGHTS_API ALL_PlayerControllerBase : public APlayerController
 	GENERATED_BODY()
 
 	protected:
-	
+
+
+	UPROPERTY(EditDefaultsOnly,Category="UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	UUserWidget* PauseMenuInstance;
+	///////////	//////////////////
 	virtual void BeginPlay() override;
 	
+	UFUNCTION(BlueprintCallable, Category="Player Controller")
+	void DisableAllInput();
+
+	UFUNCTION(BlueprintCallable, Category="Player Controller")
+	void ChangeToGameInput();
+
+	UFUNCTION(BlueprintCallable, Category="Player Controller")
+	void ChangeToUIInput();
+
+	UFUNCTION(BlueprintCallable,Category="Player Controller")
+	void TogglePauseMenu();
+	
+	virtual void SetupInputComponent() override;
 };
