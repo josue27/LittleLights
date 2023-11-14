9406d9e50f87ccd741634f8c69888ce9815bd4ed
Josue G.Z <iosue27@gmail.com>
2023-11-13 23:27:13 -0600

    Refactor UI and code dialoguesystem
    -Start of Dialogue Subsystem

---
+++ Source/LittleLights/Private/DialogueSystem/LL_DialogueSubsystem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueSystem/LL_DialogueComponent.h"
+#include "Engine/DataTable.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "LL_DialogueSubsystem.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class LITTLELIGHTS_API ULL_DialogueSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+	UDataTable* CharactersData;
+	/**
+	 * @brief For the moment the idea is to get the general data for dialogues
+	 */
+	UFUNCTION(BlueprintCallable)
+	void GetCharacterData(const ELL_CharacterID SpeakerID, FLL_DialogueSpeakers& CharacterData);
+
+	
+};
