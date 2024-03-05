47141cd9cac335f9a722560ed8956ae0ac78084d
Josue G.Z <iosue27@gmail.com>
2024-03-04 22:18:54 -0600

    Refactored tutorial dialogues trigger
    -Also added gitignore to rider files

--- Source/LittleLights/LittleLights.Build.cs
+++ Source/LittleLights/LittleLights.Build.cs
@@ -8,7 +8,7 @@ public class LittleLights : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" ,"AIModule","GameplayTasks","UMG" , "XMPP", "GameplayTags","NavigationSystem"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" ,"AIModule","GameplayTasks","UMG" , "XMPP", "GameplayTags","NavigationSystem","LevelSequence"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
